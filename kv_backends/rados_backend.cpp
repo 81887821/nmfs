41b5b7ad2b6e63761639b4eb66bfdcf19bc7c0bf
CHOKYUJIN <bori19960@naver.com>
2020-01-14 06:54:37 +0000

    fix get()

--- kv_backends/rados_backend.cpp
+++ kv_backends/rados_backend.cpp
@@ -3,7 +3,6 @@
 #include <cstring>
 #include <stdexcept>
 
-
 #include "rados_backend.hpp"
 
 #include "../constants.hpp"
@@ -11,6 +10,7 @@
 nmfs::owner_slice nmfs::kv_backends::rados_backend::get(const nmfs::slice& key) {
     uint64_t object_size;
     time_t object_mtime;
+    librados::bufferlist read_buffer;
     int ret;
 
     ret = io_ctx.stat(key.data(), &object_size, &object_mtime); // 0 on success, negative error code on failure
@@ -21,15 +21,39 @@ nmfs::owner_slice nmfs::kv_backends::rados_backend::get(const nmfs::slice& key) 
         std::cout << "rados_backend::get : Successfully write the object on " << key.data() << std::endl;
     }
 
-
     nmfs::owner_slice slice_buffer(object_size);
+
+    ret = io_ctx.read(key.data(), read_buffer, object_size, 0);
+    if (ret < 0) {
+        std::cerr << "rados_backend::get : Cannot perform partial read from object on " << key.data() << std::endl;
+        exit(EXIT_FAILURE);
+    } else {
+        std::cout << "rados_backend::get : Successfully write the object on " << key.data() << std::endl;
+    }
+
+    std::memcpy(slice_buffer.data(), read_buffer.c_str(), read_buffer.length());
+    slice_buffer.set_size(object_size);
 
     return slice_buffer;
 }
 
 void nmfs::kv_backends::rados_backend::get(const nmfs::slice& key, nmfs::slice& value) { // fully read
+    uint64_t object_size;
+    time_t object_mtime;
     librados::bufferlist read_buffer;
     int ret;
+
+    ret = io_ctx.stat(key.data(), &object_size, &object_mtime); // 0 on success, negative error code on failure
+    if (ret < 0) {
+        std::cerr << "rados_backend::get : Cannot perform read from object on " << key.data() << std::endl;
+        exit(EXIT_FAILURE);
+    } else {
+        std::cout << "rados_backend::get : Successfully write the object on " << key.data() << std::endl;
+    }
+
+    if (object_size > value.capacity()) {
+        throw std::out_of_range("rados_backend::get : capacity of value slice is not enough");
+    }
 
     ret = io_ctx.read(key.data(), read_buffer, value.capacity(), 0); // number of bytes read on success, negative error code on failure
     if (ret < 0) {
@@ -40,6 +64,7 @@ void nmfs::kv_backends::rados_backend::get(const nmfs::slice& key, nmfs::slice& 
     }
 
     std::memcpy(value.data(), read_buffer.c_str(), read_buffer.length());
+    value.set_size(read_buffer.length());
 }
 
 void nmfs::kv_backends::rados_backend::get(const nmfs::slice& key, off_t offset, size_t length, nmfs::slice& value) { // partial read
@@ -47,7 +72,7 @@ void nmfs::kv_backends::rados_backend::get(const nmfs::slice& key, off_t offset,
     int ret;
 
     if (length > value.capacity()) {
-        throw std::out_of_range("rados_backend::get : returned value size exceeds capacity of value");
+        throw std::out_of_range("rados_backend::get : returned object size exceeds capacity of value slice");
     }
 
     ret = io_ctx.read(key.data(), read_buffer, length, offset); // number of bytes read on success, negative error code on failure
@@ -57,14 +82,9 @@ void nmfs::kv_backends::rados_backend::get(const nmfs::slice& key, off_t offset,
     } else {
         std::cout << "rados_backend::get : Successfully write the object on " << key.data() << std::endl;
     }
-    /*
-    if (ret < value.capacity()) {
-        throw std::out_of_range("rados_backend::get : returned value size exceeds capacity of value");
-    } else {
-        std::memcpy(value.data(), read_buffer.c_str(), read_buffer.length());
-    }
-     */
+
     std::memcpy(value.data(), read_buffer.c_str(), read_buffer.length());
+    value.set_size(length);
 }
 void nmfs::kv_backends::rados_backend::put(const nmfs::slice& key, const nmfs::slice& value) { // fully write
     librados::bufferlist write_buffer;
@@ -96,8 +116,6 @@ void nmfs::kv_backends::rados_backend::put(const nmfs::slice& key, off_t offset,
 }
 
 bool nmfs::kv_backends::rados_backend::exist(const nmfs::slice& key) {
-    //auto ptr = std::shared_ptr<uint64_t>(new uint64_t);
-    //uint64_t* p = ptr.get();
     uint64_t object_size;
     time_t object_mtime;
     int ret;
